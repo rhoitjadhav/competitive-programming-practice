b8c9c7bd37ad67c22b8f765b1174db455a894c2e
rhoitjadhav <jadhavrohit497@gmail.com>
2021-01-24 19:47:24 +0530

    Nesting Depth

---
+++ codejam/2020/Nesting Depth.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solve(self, case, digits):
+        result = ''
+        brackets = 0
+
+        for digit in digits:
+            num = int(digit)
+
+            if num > brackets:
+                result += '(' * (num - brackets)
+                brackets = num
+
+            if num < brackets:
+                result += ')' * (brackets - num)
+                brackets = num
+
+            result += str(num)
+
+        result += ')' * brackets
+
+        print(f'Case #{case+1}: {result}')
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    tests = int(input())
+
+    for i in range(tests):
+        digits = input()
+        result = sol.solve(i, digits)
