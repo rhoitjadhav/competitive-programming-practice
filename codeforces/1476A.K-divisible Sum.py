84249e49995c9e57b83463087d7bb09c1f8ac3d0
rhoitjadhav <jadhavrohit497@gmail.com>
2021-01-31 13:12:48 +0530

    K-Divisible Sum

---
+++ codeforces/1476A.K-divisible Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def solve(self, n, k):
+        if k == 1:
+            result = 1
+
+        elif k >= n:
+            result = k // n
+            if k % n != 0:
+                result += 1
+
+        elif n % k == 0:
+            result = 1
+        
+        else:
+            result = 2
+
+        print(result)
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    tests = int(input())
+    for i in range(tests):
+        n, k = list(map(int, input().split()))
+        result = sol.solve(n, k)
