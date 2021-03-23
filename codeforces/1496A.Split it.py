6997cd2094f4f95f5e0db39045d5481550e6c0fb
rhoitjadhav <jadhavrohit497@gmail.com>
2021-03-23 17:39:07 +0530

    Split it

---
+++ codeforces/1496A.Split it.py
@@ -0,0 +1,27 @@
+class Solution:
+    def solve(self, n, k, string):
+        if (n == 0) or (n / 2 == k):
+            return "NO"
+        
+        if (k == 0):
+            return "YES"
+
+        for i in range(k):
+            if string[i] != string[n - i - 1]:
+                return "NO"
+
+        return "YES"
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    tests = int(input())
+    results = []
+    for test in range(tests):
+        n, k = list(map(int, input().split()))
+        string = input()
+        result = sol.solve(n, k, string)
+        results.append(result)
+
+    for result in results:
+        print(result)
